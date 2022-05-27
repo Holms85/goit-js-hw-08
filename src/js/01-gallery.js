6f171ffbace3dabc876ddf9d46492ad4359269de
Taras Shapovalov <tarasshapovalov85@gmail.com>
2022-05-27 22:41:11 +0300

    task1 and bit task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+const galleryRefs = document.querySelector(".gallery");
+// galleryRefs.addEventListener("click", onModalClick);
+
+galleryRefs.insertAdjacentHTML("beforeend", createElement(galleryItems));
+
+function createElement(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    })
+    .join("");
+}
+new SimpleLightbox(".gallery a", { captionsData: "alt", captionsDelay: 250 });
