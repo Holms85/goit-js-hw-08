797eaf677a5bfd5fdcd3f6f7baddc9e906887251
Taras Shapovalov <tarasshapovalov85@gmail.com>
2022-05-28 19:12:25 +0300

    finish task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+// // const player = new Vimeo.Player(iframe);
 
-const player = new Player("vimeo-player");
+const onPlay = function(data) {
+    //    console.log(data.seconds);
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+    };
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
